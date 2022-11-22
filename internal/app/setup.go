76fee04cac0e206695edb9e51171b713e4ab2ffe
David Zhang <crispgm@gmail.com>
2022-11-22 13:57:51 +0800

    feat: dashboard support options

--- internal/app/setup.go
+++ internal/app/setup.go
@@ -1,8 +1,6 @@
 package app
 
 import (
-	"fmt"
-
 	"github.com/gin-gonic/gin"
 	"github.com/osteele/liquid"
 )
@@ -16,7 +14,7 @@ func (app Application) Setup(c *gin.Context) {
 		"pageTitle": "Setup",
 		"highlight": true,
 
-		"hostname": fmt.Sprintf("https://%s", c.Request.Host),
+		"hostname": c.Request.Host,
 		"token":    app.conf.HTTP.AuthUser.Token,
 	})
 }
