545fd0fd7e3393123784d070c7ea374779c60360
David Zhang <crispgm@gmail.com>
2022-11-24 21:29:22 +0800

    ci: setup github action

--- internal/infra/db_test.go
+++ internal/infra/db_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestLoadDB(t *testing.T) {
 	db, err := LoadDB(DBConf{
-		Provider: "mysql",
+		Provider: "sqlite",
 		Name:     "read_track_dev.db",
 	})
 	if assert.NoError(t, err) {
